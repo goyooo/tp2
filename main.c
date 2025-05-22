b4c55fdd30b67955fb24b204338e3f29d24c454e
goyooo <133908348+goyooo@users.noreply.github.com>
2025-05-21 23:46:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,43 +11,94 @@ int main() {
     // ser borrado / modificado.
 
     // strLen
-    int len = strLen("hola");
-    printf("strLen(\"hola\") -> \"%i\"\n", len);
+    int len1 = strLen("");
+    printf("strLen(\"nada\") -> \"%i\"\n", len1);
+    printf("\n");
+
+    int len2 = strLen("v");
+    printf("strLen(\"v\") -> \"%i\"\n", len2);
+    printf("\n");
+
+    int len3 = strLen("ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789áéíóú");
+    printf("strLen(\"ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789áéíóú\") -> \"%i\"\n", len3);
     printf("\n");
 
     // strDup
-    char* dup = strDup("hola");
-    printf("strDup(\"hola\") -> \"%s\"\n", dup);
-    free(dup);
+    char* dup1 = strDup("");
+    printf("strDup(\"nada\") -> \"%s\"\n", dup1);
+    free(dup1);
+    printf("\n");
+
+    char* dup2 = strDup("v");
+    printf("strDup(\"v\") -> \"%s\"\n", dup2);
+    free(dup2);
+    printf("\n");
+
+    char* dup3 = strDup("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789áéíóú");
+    printf("strDup(\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789áéíóú\") -> \"%s\"\n", dup3);
+    free(dup3);
     printf("\n");
 
     // strCmp
-    char* a = "manuel";
-    char* b = "manu";
-    int cmp = strCmp(a,b);
-    printf("strComp(\"manuel, manu\") -> \"%i\"\n", cmp);
+    char* a1 = "";
+    char* b1 = "";
+    int cmp1 = strCmp(a1,b1);
+    printf("strComp(\"nada, nada\") -> \"%i\"\n", cmp1);
+    printf("\n");
+
+    char* a2 = "m";
+    char* b2 = "m";
+    int cmp2 = strCmp(a2,b2);
+    printf("strComp(\"m, m\") -> \"%i\"\n", cmp2);
+    printf("\n");
+
+    char* a3 = "manuel";
+    char* b3 = "mamuel";
+    int cmp31 = strCmp(a3,b3);
+    int cmp32 = strCmp(b3,a3);
+    printf("strComp(\"manuel, mamuel\") -> \"%i\"\n", cmp31);
+    printf("strComp(\"mamuel, manuel\") -> \"%i\"\n", cmp32);
+    printf("\n");
+
+    char* a4 = "manuel";
+    char* b4 = "manu";
+    int cmp4 = strCmp(a4,b4);
+    int cmp4 = strCmp(b4,a4);
+    printf("strComp(\"manuel, juanmanuel\") -> \"%i\"\n", cmp4);
     printf("\n");
 
     // pathNew
-    struct path* p = pathNew();
+    struct path* addf = pathNew();
+    struct path* addl = pathNew();
+    struct path* dupp = pathNew();
 
     // pathAddFirst
-    pathAddFirst(p, "Paris", 48.857, 2.352);
-    pathAddFirst(p, "Buenos Aires", -34.600, -58.382);
+    pathAddFirst(addf, "Paris", 48.857, 2.352);
+    pathAddFirst(addf, "Obera", 27.486, 55.12);
+    pathAddFirst(addf, "Rauch", -36.774, -59.089);
 
     // pathAddLast
-    pathAddLast(p, "Madrid", 40.417, -3.703);
-    pathAddLast(p, "Tokio", 35.689, 139.692);
-
-    // pathPrint
-    //pathPrint(p);
-
+    pathAddLast(addl, "Rauch", -36.774, -59.089);
+    pathAddLast(addl, "Moose", 46.655, -11.0717);
+    pathAddLast(addl, "Ombada", 15.680, 32.316);
+    
     // pathDuplicate
-    struct path* np = pathDuplicate(p);
+    struct path* np = pathDuplicate(dupp);
     pathPrint(np);
+    printf("\n");
+    pathAddFirst(dupp, "Rauch", -36.774, -59.089);
+    struct path* np1 = pathDuplicae(dupp);
+    pathPrint(np);
+    printf("\n");
+    pathAddLast(dupp, "Togusken", 48.371, 69.994);
+    struct path* np2 = pathDuplicate(dupp);
+    pathPrint(np);
+    printf("\n");
+
+    //pathPrint(np);
 
     // pathSwapStops
-    pathSwapStops(np, "Buenos Aires", "Tokio");
+    pathSwapStops(np, "Buenos Aires", "Rauch");
     pathPrint(np);
 
     // pathRemoveCity
@@ -55,8 +106,12 @@ int main() {
     //pathPrint(p);
 
     // pathDelete
-    pathDelete(p);
+    pathDelete(addf);
+    pathDelete(addl);
+    pathDelete(dupp);
     pathDelete(np);
+    pathDelete(np1);
+    pathDelete(np2);
 
     return 0;
 }
