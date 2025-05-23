eb7092d720f88d7901beab5feede7cb66a6785ec
goyooo <133908348+goyooo@users.noreply.github.com>
2025-05-23 17:56:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -70,7 +70,10 @@ int main() {
     pathAddFirst(addf, "Oberá", 27.486, 55.12);
     //añadir un nodo a un path con más de una parada
     pathAddFirst(addf, "Rauch", -36.774, -59.089);
-    
+    printf("pathAddFirst");
+    pathPrint(addf);
+    printf("\n");
+
     // pathAddLast
     struct path* addl = pathNew();
     //añadir un nodo a una path vacío
@@ -79,8 +82,12 @@ int main() {
     pathAddLast(addl, "Moose", 46.655, -11.0717);
     //añadir un nodo a un path con más de una parada
     pathAddLast(addl, "Ombada", 15.680, 32.316);
+    printf("pathAddLast");
+    pathPrint(addl);
+    printf("\n");
     
     // pathDuplicate
+    printf("pathDuplicate");
     struct path* dupp = pathNew();
     //duplicar un path vacío
     struct path* np = pathDuplicate(dupp);
@@ -91,30 +98,38 @@ int main() {
     struct path* np1 = pathDuplicate(dupp);
     pathPrint(np1);
     printf("\n");
-    //duplicar un path con má de una parada
+    //duplicar un path con más de una parada
     pathAddLast(dupp, "Togusken", 48.371, 69.994);
     struct path* np2 = pathDuplicate(dupp);
     pathPrint(np2);
     printf("\n");
 
-
     // pathSwapStops
+    printf("pathSwapStops");
     struct path* swapswap = pathNew();
     pathAddFirst(swapswap, "Venado Tuerto", -33.746, -61.960);
     pathAddFirst(swapswap, "Monte Buey", -32.918, -62.461);
+    printf("original \n");
+    pathPrint(swapswap);
+    printf("\n");
 
     //Swap de ciudades con mismo nombre
     pathSwapStops(swapswap, "Venado Tuerto", "Venado Tuerto");
+    printf("no deberia hacer nada\n");
     pathPrint(swapswap);
     printf("\n");
+
     //Swap de dos ciudades en un path con solo dos ciudades
     pathSwapStops(swapswap, "Venado Tuerto", "Monte Buey");
+    printf("al vesre\n");
     pathPrint(swapswap);
     printf("\n");
+
     //Swap de dos ciudades tal que sean la primera y la ultima del camino
     pathAddLast(swapswap, "Oberá", 27.486, 55.12);
     pathAddLast(swapswap, "Rauch", -36.774, -59.089);
 
+    printf("primera y ult\n");
     pathSwapStops(swapswap, "Venado Tuerto", "Rauch");
     pathPrint(swapswap);
     printf("\n");
@@ -125,20 +140,27 @@ int main() {
     pathAddLast(rem, "Bergamo", 45.695, 9.671);
     pathAddLast(rem, "Kracovia", 50.061, 19.947);
     pathAddLast(rem, "Emmeloord", 52.708, 5.751);
+    printf("original pathRemoveCity \n");
+    pathPrint(rem);
+    printf("\n");
+
 
     //Borrar primer ciudad de un camino de cuatro ciudades
     pathRemoveCity(rem, "Lieja");
+    printf("borrar lieja, primera ciudad \n");
     pathPrint(rem);
     printf("\n");
 
     pathAddFirst(rem,"Palmas", -10.259, -48.328);
     //Borrar la última ciuada de un camino de cuatro ciudades
-    pathRemoveCity(rem, "Emmerlood");
+    printf("borra Emmeloord, la ultima ciudad \n");
+    pathRemoveCity(rem, "Emmeloord");
     pathPrint(rem);
     printf("\n");
 
     //Borrar una ciudad intermedia de un camino de cuatro ciudades
     pathAddFirst(rem, "Bielefeld", 52.020, 8.535);
+    printf("borra kracovia del medio \n");
     pathRemoveCity(rem, "Kracovia");
     pathPrint(rem);
     printf("\n");
@@ -147,6 +169,7 @@ int main() {
     pathRemoveCity(rem, "Bergamo");
     pathRemoveCity(rem, "Palmas");
     //queda solo Bielefeld
+    printf("borra Bielefeld que deberia ser la unica \n");
     pathRemoveCity(rem, "Bielefeld");
     pathPrint(rem);
     printf("\n");
